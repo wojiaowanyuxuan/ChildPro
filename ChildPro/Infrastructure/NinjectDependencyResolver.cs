e1e02aebfd6dc8a76d1e51c08ed6dfe5ea866150
万宇轩 <yuxuan1491433427@qq.com>
2019-04-21 20:13:33 +0800

    第一次提交

---
+++ ChildPro/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace ChildPro.Infrastructure
+{
+	public class NinjectDependencyResolver
+	{
+		private IKernel kernel;
+		public NinjectDependencyResolver(IKernel kernelParam)
+		{
+			kernel = kernelParam;
+			//添加绑定
+			AddBindings();
+		}
+		public object GetService(Type serviceType)
+		{
+			return kernel.TryGet(serviceType);
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return kernel.GetAll(serviceType);
+		}
+		private void AddBindings()
+		{
+
+		}
+	}
+}
