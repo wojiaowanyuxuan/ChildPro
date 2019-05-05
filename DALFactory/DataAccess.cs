ac42ef873ea62ce0da940e3dc1714daa2b1e21af
万宇轩 <yuxuan1491433427@qq.com>
2019-05-05 19:55:14 +0800

    搭好框架

---
+++ DALFactory/DataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DALFactory
+{
+	public class DataAccess
+	{
+		//程序集的名称
+		private static string AssemblyName = ConfigurationManager.AppSettings["Path"].ToString();
+		//数据库名称
+		private static string db = ConfigurationManager.AppSettings["DB"].ToString();
+
+	}
+}
